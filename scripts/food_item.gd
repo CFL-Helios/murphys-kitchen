6f61bb1b1b005e78f160f6b30eef09727846ce09
Kevin Zhan <kevin.w.zhan@gmail.com>
2025-02-21 21:23:53 -0500

    fix food carry issue

--- scripts/food_item.gd
+++ scripts/food_item.gd
@@ -6,16 +6,14 @@ var food_meshes : Array[MeshInstance3D]
 func _ready() -> void:
 	var children = get_children()
 	for child in children:
-		print(child)
 		if child is MeshInstance3D:
 			food_meshes.push_back(child)
-			
-			
 
 func get_meshes() -> Array[MeshInstance3D]:
 	return food_meshes
 
 func _on_body_entered(body: Node) -> void:
+	print(body.name)
 	if body.is_in_group("Food Safe"): return
 	
 	splatter.reparent(body)
