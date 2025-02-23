47da6ccb5e1d9041bd5a992f293737b82466d5c7
Kevin Zhan <kevin.w.zhan@gmail.com>
2025-02-23 01:54:14 -0500

    fix weird scene transition issue

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -2,8 +2,6 @@ class_name EndScreen extends CanvasLayer
 
 @onready var scoreboard : RichTextLabel = $ColorRect/PanelContainer/MarginContainer/VBoxContainer/Score
 
-var main_menu : PackedScene = preload("res://levels/start_menu.tscn")
-
 func _ready() -> void:
 	hide()
 
@@ -11,12 +9,10 @@ func set_score(score : int) -> void:
 	scoreboard.text = "Score : " + str(score)
 
 func _on_main_menu_pressed() -> void:
-	print("return to main")
 	get_tree().paused = false
-	get_tree().change_scene_to_packed(main_menu)
+	get_tree().change_scene_to_file("res://levels/start_menu.tscn")
 
 func _on_play_again_pressed() -> void:
-	print("play again")
 	get_tree().paused = false
 	ScoreManager.score = 0
 	get_tree().reload_current_scene()
